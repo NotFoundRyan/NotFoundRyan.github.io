2044d26f953921bcc77b768cb83d2ba289d97e8f
chenpengrui <1394872816@qq.com>
2025-12-15 22:50:50 +0800

    Site updated: 2025-12-15 22:50:49

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
-/* main function */
-import initUtils from "./utils.js";
-import initTyped from "./plugins/typed.js";
-import initModeToggle from "./tools/lightDarkSwitch.js";
-import initLazyLoad from "./layouts/lazyload.js";
-import initScrollTopBottom from "./tools/scrollTopBottom.js";
-import initLocalSearch from "./tools/localSearch.js";
-import initCopyCode from "./tools/codeBlock.js";
-import initBookmarkNav from "./layouts/bookmarkNav.js";
-import initParticles from "./build/layouts/setup-particles.js";
-// import { emojiCursor } from "cursor-effects";
-// console.log(emojiCursor);
-// new emojiCursor({ emoji: ["🔥", "🐬", "🦆"] });
+/*
+ * Redefine theme main entry point
+ *
+ * @package: @redefinejs\theme
+ * @author:  Ryan Chen
+ * @link:    https://github.com/NotFoundRyan/hexo-theme-redefine
+ */
+import * as utils from './utils/index.js';
+import * as tools from './tools/index.js';
+import theme from './config/theme.js';
 
 export const main = {
   themeInfo: {
     theme: `Redefine v${theme.version}`,
-    author: "EvanNotFound",
-    repository: "https://github.com/EvanNotFound/hexo-theme-redefine",
+    author: "Ryan Chen",
+    repository: "https://github.com/NotFoundRyan/hexo-theme-redefine",
   },
   localStorageKey: "REDEFINE-THEME-STATUS",
   styleStatus: {
@@ -27,7 +24,7 @@ export const main = {
   },
   printThemeInfo: () => {
     console.log(
-      `      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\\\/____\/ \\\/_____\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine`,
+      `      ______ __  __  ______  __    __  ______                       \r\n     \/__  _\/\ \_\ \/\  ___\/\ "-.\/  \/\  ___\                      \r\n     \\/_\/\ \\ \  __ \ \  __\\ \ \-.\/\ \ \  __\                      \r\n        \ \_\\ \_\ \_\ \_____\ \_\ \ \_\ \_____\                    \r\n         \\/_\/ \\/_\/\/_\/_____\/_\/  \\/_\/\/_____\                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\  == \\/\  ___\/\  __-.\/\  ___\/\  ___\/\ \\/\ "-.\ \\/\  ___\   \r\n\\ \  __<\\ \  __\\ \ \/\ \ \  __\\ \  __\ \ \ \ \-.  \ \  __\   \r\n \ \_\ \_\ \_____\ \____-\ \_____\ \_\  \ \_\ \_\\"\_\ \_____\ \r\n  \\/_\/ \/_\/_____\/____\/ \/_____\/\/   \\/_\/\/\/_\/\/\/_____\r\n                                                               \r\n  Github: https:\/\/github.com\/NotFoundRyan\/hexo-theme-redefine`,
     ); // console log message
   },
   setStyleStatus: () => {
@@ -48,46 +45,29 @@ export const main = {
       return null;
     }
   },
-  refresh: () => {
-    initUtils();
-    initModeToggle();
-    initScrollTopBottom();
-    initBookmarkNav();
-
-    if (theme.effects.particles === true) {
-      initParticles();
-    }
-
-    if (
-      theme.home_banner.subtitle.text.length !== 0 &&
-      location.pathname === config.root
-    ) {
-      initTyped("subtitle");
-    }
-
-    if (theme.navbar.search.enable === true) {
-      initLocalSearch();
-    }
-
-    if (theme.articles.code_block.copy === true) {
-      initCopyCode();
-    }
-
-    if (theme.articles.lazyload === true) {
-      initLazyLoad();
-    }
+  setStyleStatusToDefault: () => {
+    localStorage.setItem(
+      main.localStorageKey,
+      JSON.stringify({
+        isDark: theme.colors.default_mode && theme.colors.default_mode === "dark",
+        fontSizeLevel: 0,
+        isExpandPageWidth: false,
+        isOpenPageAside: true,
+      }),
+    );
+    main.getStyleStatus();
+  },
+  init: () => {
+    main.printThemeInfo();
+    main.getStyleStatus();
+    utils.init();
+    tools.init();
   },
 };
 
-export function initMain() {
-  main.printThemeInfo();
-  main.refresh();
+// Initialize theme when DOM is ready
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", main.init);
+} else {
+  main.init();
 }
-
-document.addEventListener("DOMContentLoaded", initMain);
-
-try {
-  swup.hooks.on("page:view", () => {
-    main.refresh();
-  });
-} catch (e) { }
